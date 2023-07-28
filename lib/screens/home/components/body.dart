3fbf9d1648508d2a1982aca3f640e1164746211a
songhee24 <akaratel61@gmail.com>
2023-07-28 17:52:20 +0600

    Search Input Done

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -21,7 +21,40 @@ class Body extends StatelessWidget {
                     bottomRight: Radius.circular(36),
                   ),
                 ),
-              )
+              ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  height: 54,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: const Offset(0, 10),
+                        blurRadius: 50,
+                        color: kPrimaryColor.withOpacity(0.23),
+                      )
+                    ],
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Search",
+                      hintStyle: TextStyle(
+                        color: kPrimaryColor.withOpacity(0.5),
+                      ),
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         )
