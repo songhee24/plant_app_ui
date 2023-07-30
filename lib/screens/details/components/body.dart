fa4ef0f002c1b13f388258395342ee6a8563cb24
songhee24 <akaratel61@gmail.com>
2023-07-31 05:56:33 +0600

    Under Text Styles

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app_ui/constants.dart';
 import 'package:plant_app_ui/screens/details/components/image_with_icons.dart';
 
 ///  Created by mac on 15/8/23.
@@ -10,6 +11,37 @@ class Body extends StatelessWidget {
       child: Column(
         children: [
           ImageWithIcons(size: size),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+            child: Row(
+              children: [
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'Angelica\n',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineMedium
+                            ?.copyWith(
+                              color: kTextColor,
+                              fontWeight: FontWeight.bold,
+                            ),
+                      ),
+                      const TextSpan(
+                        text: 'Russia',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
