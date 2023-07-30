ac036931d8fb2f8fb5c69d4847160335895640f9
songhee24 <akaratel61@gmail.com>
2023-07-31 05:22:47 +0600

    starting decorting image inside

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -4,6 +4,32 @@ import 'package:flutter/material.dart';
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: Column(),
+            ),
+            Container(
+              height: size.height * 0.8,
+              width: size.width * 0.75,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(63),
+                  bottomLeft: Radius.circular(63),
+                ),
+                image: DecorationImage(
+                  alignment: Alignment.centerLeft,
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/images/img.png'),
+                ),
+              ),
+            )
+          ],
+        )
+      ],
+    );
   }
 }
--- lib/screens/details/details_screen.dart
+++ lib/screens/details/details_screen.dart
@@ -3,6 +3,8 @@ import 'package:plant_app_ui/screens/details/components/body.dart';
 
 ///  Created by mac on 15/8/23.
 class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
