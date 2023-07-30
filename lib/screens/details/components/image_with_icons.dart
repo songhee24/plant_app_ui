8c9fb0d99d84c665c9d6bc98dbed0be3834eee54
songhee24 <akaratel61@gmail.com>
2023-07-31 05:51:58 +0600

    Extracting to new files

---
+++ lib/screens/details/components/image_with_icons.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:plant_app_ui/constants.dart';
+import 'package:plant_app_ui/screens/details/components/icon_card.dart';
+
+///  Created by mac on 15/8/23.
+class ImageWithIcons extends StatelessWidget {
+  final Size size;
+
+  const ImageWithIcons({super.key, required this.size});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: kDefaultPadding * 3),
+      child: SizedBox(
+        height: size.height * 0.8,
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: kDefaultPadding * 3),
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: IconButton(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: kDefaultPadding),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: SvgPicture.asset(
+                          'assets/icons/back_arrow.svg',
+                        ),
+                      ),
+                    ),
+                    const Spacer(),
+                    IconCard(size: size, icon: 'assets/icons/sun.svg'),
+                    IconCard(size: size, icon: 'assets/icons/icon_2.svg'),
+                    IconCard(size: size, icon: 'assets/icons/icon_3.svg'),
+                    IconCard(size: size, icon: 'assets/icons/icon_4.svg'),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: size.height * 0.8,
+              width: size.width * 0.75,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(63),
+                  bottomLeft: Radius.circular(63),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    offset: const Offset(0, 10),
+                    blurRadius: 60,
+                    color: kPrimaryColor.withOpacity(0.30),
+                  ),
+                ],
+                image: const DecorationImage(
+                  alignment: Alignment.centerLeft,
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/images/img.png'),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
